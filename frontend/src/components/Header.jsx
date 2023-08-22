37dd5007f1f8bd99443f50afbe6cc6f789751691
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-08-21 21:15:27 -0300

    Footer/Header created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import {FaUser, FaShoppingCart} from 'react-icons/fa'
 
+
 const Header = () => {
   return (
-    <header className='flex items-center'>
-      <h1>
+    <header className='flex items-center w-auto h-24 bg-red-500 '>
+      <h1 className='flex grow text-xl ml-16'>
         Plaisir
       </h1>
+      <div className='flex items-center space-x-4 mr-4'>
+        <span>Carrinho</span>
       <FaShoppingCart/>
+      <span>Faça seu login</span>
       <FaUser/> 
+      </div>
     </header>
   )
 }
