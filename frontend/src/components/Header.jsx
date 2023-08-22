52a7b595b9eb2d8db1bf3c8adef3ece61408ce7a
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-08-21 22:01:20 -0300

    Update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import {FaUser, FaShoppingCart} from 'react-icons/fa'
-
+import logo from '../assets/logo.jpg'
 
 const Header = () => {
   return (
-    <header className='flex items-center w-auto h-24 bg-red-500 '>
+    <header className='flex items-center w-auto h-28 bg-rosa '>
+    <img src={logo} alt='logo' className='w-auto h-full '/>
       <h1 className='flex grow text-xl ml-16'>
         Plaisir
       </h1>
       <div className='flex items-center space-x-4 mr-4'>
-        <span>Carrinho</span>
+      <div className='flex items-center space-x-4 cursor-pointer hover:text-white'>
+      <span>Carrinho</span>
       <FaShoppingCart/>
+      </div>
+      <div className='flex items-center space-x-4 cursor-pointer hover:text-white'>
       <span>Faça seu login</span>
       <FaUser/> 
+      </div>
       </div>
     </header>
   )
